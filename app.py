cc43a9293deb52ef22361358e9ab5dcbe8e348c1
Rafael <rafaelribeirodasilva7@mail.com>
2026-05-26 20:16:49 -0300

    novos arquvos de template html dashboard e login

--- app.py
+++ app.py
@@ -10,5 +10,13 @@ def home():
 def sobre():
     return render_template('sobre.html')
 
+@app.route('/dashboard', methods=['POST'])
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__=="__main__":
     app.run(port=5000, host="0.0.0.0", debug=True)
